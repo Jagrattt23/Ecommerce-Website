2369c10f28d2952a4a5d79bfdc58fd2939dbbfed
Jagrattt23 <jagratmalviya99@gmail.com>
2025-01-30 23:39:42 +0530

    Enabled DEBUG mode for debugging

--- Ecommerce/settings.py
+++ Ecommerce/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-m-zwk-pmzqs2&fri61r2)nb$o+-k!n61)ej(9qcsyrz)d8o43%"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 
